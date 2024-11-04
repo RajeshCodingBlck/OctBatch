72c4cf4169d76c1f5685e6e6bceeaa7d5aa984cc
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-11-04 20:16:20 +0530

    Add files via upload

---
+++ Practise1/sample.java
@@ -0,0 +1,15 @@
+package Practise1;
+
+public class sample {
+
+
+    public void fun(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        
+        int a;
+
+        //System.out.println(a);
+    }
+}
