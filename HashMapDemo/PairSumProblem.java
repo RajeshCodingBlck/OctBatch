43ff63e206f55c17f97aa1b28d684173439bcb7c
Rajesh <rajeshguptamc102@gmail.com>
2025-03-29 19:30:24 +0530

    HashMap and LinkedList

--- HashMapDemo/PairSumProblem.java
+++ HashMapDemo/PairSumProblem.java
@@ -9,23 +9,40 @@ public class PairSumProblem {
 		HashMap<Integer, Integer> hm= new HashMap();
 		
 		int ans=0;
-		for(int i=arr.length-1; i>=0;i--) {
-			
-			int secondValue= target-arr[i];
-			if(hm.containsKey(secondValue)) {
-				int oldFre= hm.get(secondValue);
-				ans+=oldFre;
-			}
-			
-			if(hm.containsKey(arr[i])) {
-				int oldFre= hm.get(arr[i]);
-				hm.put(arr[i],oldFre+1);
-			}else {
-				hm.put(arr[i], 1);
-			}
-		}
-		return ans;
+//		for(int i=arr.length-1; i>=0;i--) {
+//			
+//			int secondValue= target-arr[i];
+//			if(hm.containsKey(secondValue)) {
+//				int oldFre= hm.get(secondValue);
+//				ans+=oldFre;
+//			}
+//			
+//			if(hm.containsKey(arr[i])) {
+//				int oldFre= hm.get(arr[i]);
+//				hm.put(arr[i],oldFre+1);
+//			}else {
+//				hm.put(arr[i], 1);
+//			}
+//		}
+//		return ans;
 		
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			int firstValue= target-arr[i];
+			if(hm.containsKey(firstValue)) {
+				ans+= hm.get(firstValue);
+			}
+			
+//			if(hm.containsKey(arr[i])) {
+//				hm.put(arr[i], hm.get(arr[i])+1);
+//			}else {
+//				hm.put(arr[i], 1);
+//			}
+			
+			hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+			
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- LinkedListPackage/MiddleOfLinkedList.java
+++ LinkedListPackage/MiddleOfLinkedList.java
@@ -2,6 +2,19 @@ package LinkedListPackage;
 
 public class MiddleOfLinkedList {
 
+//	 public ListNode middleNode(ListNode head) {
+//	        
+//
+//	        ListNode fast= head;
+//	        ListNode slow=head;
+//
+//	        while(fast !=null  && fast.next !=null){
+//	            slow=slow.next;
+//	            fast=fast.next.next;
+//	        }
+//	        
+//	        return slow;
+//	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
